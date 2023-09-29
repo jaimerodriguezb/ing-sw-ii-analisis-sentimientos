e07f253e850d8a4d774ce271853518aaf85ae00e
Migue <miguelorlandomora@gmail.com>
2023-09-28 21:01:29 -0500

    Api V1

---
+++ App.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_restx import Api, Resource, fields
+from flask_cors import CORS
+
+from modelo.analisis_sentimientos import Analizador
+
+app = Flask(__name__)
+CORS(app)
+analizar = Analizador()
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Analizador de Sentimientos',
+    description='Analizador de Sentimientos')
+
+ns = api.namespace('Analizador')
+
+analizador = api.parser()
+
+analizador.add_argument(
+    'Comentario', 
+    type=str, 
+    required=True,
+    help='Ingrese un comentario', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.Integer,
+})
+
+@ns.route('/gates')
+class LogicGategApi(Resource):
+
+    @api.doc(parser=analizador)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = analizador.parse_args()
+
+        return {
+         "result": analizador.analizar(args['comentario'])
+        }, 200
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
